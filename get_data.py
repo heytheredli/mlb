218fad2a06db9bb1f3bee3c15729307b39f5049e
Daniel Li <DanielLi@Daniels-MacBook-Air-3.local>
2019-06-23 16:10:42 -0400

    changing script output

--- get_data.py
+++ get_data.py
@@ -16,4 +16,6 @@ for rawfile in config['rawfiles'].keys():
                                         for col in raw_data[rawfile]["data"].columns
                                             if col.lower() in [x.lower() for x in config['id_cols']]
                                     ]
-print(raw_data)
+for dataset in raw_data.keys():
+    print(dataset, "all cols : ", raw_data[dataset]["all_cols"])
+    print(dataset, "id cols: ", raw_data[dataset]["id_cols"])

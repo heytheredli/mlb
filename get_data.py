cedde85ff228b9047a6aee1cd970070fc8f66e4d
Daniel Li <DanielLi@Daniels-MacBook-Air-3.local>
2019-06-23 16:12:07 -0400

    removing comment

--- get_data.py
+++ get_data.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import yaml
 
-
-
 with open("config.yml", 'r') as stream:
     config = yaml.load(stream)
 
-# print(config)
 raw_data = {}
 
 for rawfile in config['rawfiles'].keys():

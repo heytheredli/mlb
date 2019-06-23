33082e7ec14b009a3d2d112a252b57360cc5b006
Daniel Li <DanielLi@Daniels-MacBook-Air-3.local>
2019-06-23 16:05:43 -0400

    creating metadata

--- get_data.py
+++ get_data.py
@@ -10,6 +10,10 @@ with open("config.yml", 'r') as stream:
 raw_data = {}
 
 for rawfile in config['rawfiles'].keys():
-    raw_data[rawfile] = pd.read_csv(config['rawfiles'][rawfile], nrows=10)
-
+    raw_data[rawfile] = {"data": pd.read_csv(config['rawfiles'][rawfile], nrows=10)}
+    raw_data[rawfile]["all_cols"] = raw_data[rawfile]["data"].columns
+    raw_data[rawfile]["id_cols"] = [col
+                                        for col in raw_data[rawfile]["data"].columns
+                                            if col.lower() in [x.lower() for x in config['id_cols']]
+                                    ]
 print(raw_data)
